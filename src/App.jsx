8aecfa6020032ef7033a1b2f3b1012b7da50c437
Anju Adhikari <anjuadhkari667@gmail.com>
2025-06-19 20:01:59 +0545

    use effect hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,28 @@ import ProfileCard from "./profileCard";
 
 function App() {
   return (
-    <ThemeProvider>
-      <ThemeSwitch />
-      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white flex flex-col items-center justify-center gap-6 p-6 transition-colors">
-        <h1 className="text-4xl font-bold">Dark Mode Theme</h1>
-        <div className="mt-6 p-6 rounded-xl shadow-xl bg-gray-100 dark:bg-gray-800 w-full max-w-md">
-          <h2 className="text-2xl font-semibold mb-2">Theme Aware Card</h2>
-          <p>This card adapts on the current theme.</p>
-        </div>
+    <div>
+      <h1 className="text-center mt-10">Jokes</h1>
 
-        {/* Uncomment and use ProfileCard if needed */}
-        {/*
-        <ProfileCard
-          name="Elon Musk"
-          bio="CEO of Tesla and SpaceX. Innovator, entrepreneur, and visionary shaping the future of space and energy."
-        />
-        */}
-      </div>
-    </ThemeProvider>
+    </div>
+    // <ThemeProvider>
+    //   <ThemeSwitch />
+    //   <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white flex flex-col items-center justify-center gap-6 p-6 transition-colors">
+    //     <h1 className="text-4xl font-bold">Dark Mode Theme</h1>
+    //     <div className="mt-6 p-6 rounded-xl shadow-xl bg-gray-100 dark:bg-gray-800 w-full max-w-md">
+    //       <h2 className="text-2xl font-semibold mb-2">Theme Aware Card</h2>
+    //       <p>This card adapts on the current theme.</p>
+    //     </div>
+
+    //     Uncomment and use ProfileCard if needed 
+        
+    //     <ProfileCard
+    //       name="Elon Musk"
+    //       bio="CEO of Tesla and SpaceX. Innovator, entrepreneur, and visionary shaping the future of space and energy."
+    //     />
+       
+    //   </div>
+    // </ThemeProvider>
   );
 }
 
